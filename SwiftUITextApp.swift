10ed355771a9d53669c7d7640c5f5af19fb2c0d7
ldur <lasse@durucz.com>
2023-10-14 15:28:52 +0200

    first

---
+++ SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by Simon Ng on 3/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
